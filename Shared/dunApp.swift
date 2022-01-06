9421b978934254fffd926eb1a64a0c5ecbd6a8a5
Luca Beetz <luca-beetz@tutanota.com>
2022-01-06 02:04:43 +0100

    Initial Commit

---
+++ Shared/dunApp.swift
@@ -0,0 +1,17 @@
+//
+//  dunApp.swift
+//  Shared
+//
+//  Created by Luca Beetz on 06.01.22.
+//
+
+import SwiftUI
+
+@main
+struct dunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
